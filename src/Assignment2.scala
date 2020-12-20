18d5d84738545a1d03fb70abf24288931f0e381d
osmeden <user@test.com>
2020-12-20 14:49:16 +0100

    Added task descriptions as commentaries

--- src/Assignment2.scala
+++ src/Assignment2.scala
@@ -2,6 +2,10 @@ object Assignment2 {
 
   def main(args: Array[String]): Unit = {
 
+    /*1. Use pattern matching in a function accepting a String.
+    For strings containing names of days of the week return “work” (Monday-Friday)
+    or “weekend” ( for weekends).
+    For all other strings return “no such day”*/
     println("Task 1")
     println(daysPatternMatch("Monday"))
     println(daysPatternMatch("Wednesday"))
@@ -9,6 +13,9 @@ object Assignment2 {
     println(daysPatternMatch("sunday"))
     println(daysPatternMatch("sandwich"))
 
+    /*2. Define a BankAccount class with methods deposits and withdraw and currentBalance read only property.
+    The class should provide a constructor accepting initial balance
+    and second constructor, setting the balance to 0.*/
     println("\nTask 2")
     val acc = new BankAccount(250000.35)
     println("account balance = " + acc.balance)
@@ -17,6 +24,11 @@ object Assignment2 {
     val acc1 = new BankAccount()
     println("account 1 balance = " + acc1.balance)
 
+    /*3. Define a Person class with properties firstName and lastName.
+    Create a few instances. Define a function accepting Person instance
+    and using pattern matching to select string greeting the person.
+    Define 2-3 different greetings for people meeting specific criteria
+    (eg. specific first or last name) and one generic greeting*/
     println("\nTask 3")
     val person1 = new Person("Mike", "Morhaime")
     val person2 = new Person("Jill", "Smith")
@@ -27,9 +39,22 @@ object Assignment2 {
     println(getGreeting(person3))
     println(getGreeting(person4))
 
+    /*4. Define a function accepting two parameters –
+    integer and a function operating on integers.
+    Apply the parameter function three times to the integer and return a result*/
     println("\nTask 4")
     println(tripleApplication(checkTheSign, -6))
 
+    /*5. Define a Person class and three traits: Student, Teacher, Employee.
+    Person should have firstName, lastName and taxToPay read only properties.
+    Employee should have a salary property (with getter and setter).
+    Student and Employee traits should override taxToPay property –
+    for Student it should return 0, for Employee it should return 20% of their salary.
+    Teacher should inherit from employee, the taxToPay should return 10% of their salary.
+    Create objects with each of the properites, show how their properties work.
+    Create two objects with both the Student and Employee properties
+    (add them in different order in each object), show how taxToPay will work with this object
+    depending on the order those traits are added upon instantiation.*/
     println("\nTask 5")
     val person = new Person("John", "Smith")
     val employee = new Person("Jake", "Johnson") with Employee {
@@ -69,7 +94,6 @@ object Assignment2 {
       case "Jill" => "Madam, nice to see you"
       case _ => "Hey there!"
     }
-
 
   class BankAccount(val currentBalance: Double) {
     private var _balance = currentBalance
